91f6fbe6e3cdaf82c0ae568fc3f6c5fc8a8629f4
Justin Steffen <jcsteffen03@gmail.com>
2024-06-03 21:58:30 -0600

    Arrow Functions

---
+++ index.js
@@ -0,0 +1,8 @@
+
+const divide = function (num1, num2) {
+    return num1 / num2;
+}
+
+const square = (parameter) => parameter * parameter;
+
+const add = (parameter1, parameter2) => parameter1 + parameter2;
